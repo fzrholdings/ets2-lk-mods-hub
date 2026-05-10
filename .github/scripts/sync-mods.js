dd3e3c776cb2867dfe95b001c85321c983df82aa
YourGitHubUsername <your-email@example.com>
2026-05-10 13:17:06 +0530

    Add sync script using GitHub secret (without hardcoded credentials)

---
+++ .github/scripts/sync-mods.js
@@ -0,0 +1,13 @@
+{
+  "type": "service_account",
+  "project_id": "lk-ets2-mods-hub-ca78e",
+  "private_key_id": "e520b496cefb2e029e9ef5261a01a4244a9e9af0",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC28ZKEsrSptS8C\nfe55zFPJ7S043Ogzq9pYbiLnv1tLl36zT+Wa5WYTt25LY1gdaF7PBeXBuXnZwwJw\n+IYEcf5nCzPb8ZeKwn0mgeQSaoJW464jRCTG3yDok0Rqc+8+bPtRCZ313lr66sNO\n/L1+YASbeRkOhlOzO+etLwYqfpPXDhG5FGDpaC6UhMXpjaa14hGMmiyM9uKV733P\nMX62Z06ZvwAhmAv7c+ciavSx6uJIpqbzX1w5QfxEJc8aOcwoGFaIgaR+6lTvZTni\nRJbRfIK4Q4Oi+xe9yvaTky1HOt4b4kZlAJxRPdUcWfzIgo74nx8VvGaLzD7bWCug\nK3658ILBAgMBAAECggEAPZeSZUPHSHW8KIIednpA4XSNK55QPQlIX0phTRXjVk1g\ndl1ZZ6+BEPVKWG5PfcMkXF3Xb0QTTqn1mfRKJpf8d1PRt2ScyTZWrU7A6yRZ0ogg\nGjZ4jU4VBciDGbYDn8hQSoXK3Trp9Z1xeRflcnVAR9nbAkVeS7NI+kkhMdBg1N+R\nX9/aAl8BYLZS9TwMJb9U3YUoo/2zM+GyvzvRd7J2zKxXyGNfi/vmJOWaMgSgeVz4\nLA8fppIatpMoUHz9OGTW1yobZLdCY17W7XBNk4TGzyN+wU6+pvn89+ZYThyVgec5\nx/5HV+LL4ergQKjMyqZx7rMy4lEz30Wp0IMfOHsiqwKBgQDidoWy54n9PDRl6asL\nlxcOi/HDoLAR1a/RNzu9gcJbvqgc29PiSH8V2UfloU89xkSA+745a+tgEz4XmO0Y\nx/3mF7fxtZKUeOvJQOk1Utyi8Wyrd9rO9cH6pmaf94oAIovDmrJmIGhaL+v0Se/9\nxVb/M1CtqsQtTEGp6Ng5tUCjgwKBgQDOzfZ1UM3paegV16vfjCmsiaztVrxeQJPg\ncf2UAi5DMdaoXtrCW8zUG4CGuudLum9bb7dSsEQDmcZDQmxHpKY1wYAOv66bFBnq\nFJg3swFInFeUWWbudFz2Z7DxqpLH45kll4P6lmwlKBGVbX/Cpm0udgwrif7ZK/xt\n7px3Uyw5awKBgQCrbH5uW5h3qcAcORIx9tAMQeDjbdp51aB+JWpCXFwMaLkHB1xP\nmQMiPdSPvVincVLftDBddRS6KDbDVA59yL2oE+I4xHJNk28ga9NcevnrGgdhkJuA\nOEX6/0UF6DJ9Qak8ivhqGDQFuOrKg8F3/o6JJYX3wCE6DNRT0p8Cp/JjEwKBgQDB\ngdFqhriV/wYuYgct/w4DpzOLRUyu+MgmDSOUudGmCF1uYItbr+BiXHWQHBVPM9z8\n9vTXx51srRfqWZ3OD8+Cn1P++Pcdntp8wNaJz9KUqc39PwZke7d8zRuxlcUQivQD\nS9ovwU7iGbMSWkpgza99+PF8T/yKI3FpQAtiUA4ijwKBgDqyGxo3nVYxdjtlg9OR\nKFCJfyfZ101c1OmRqGmyeeIJa6LN16oJe/Ph6k++KAOaXXWZICnc8LZt6X7WBANf\npOTLlmMBYKQ4gn/8ePycsvBRxix/PvnjRBY7T+GU2cgMACe4yQyXVgIjwgvF0ARp\nGk4OXNpMw3A48kVxut9w/SiZ\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-fbsvc@lk-ets2-mods-hub-ca78e.iam.gserviceaccount.com",
+  "client_id": "111154261648697877822",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40lk-ets2-mods-hub-ca78e.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
